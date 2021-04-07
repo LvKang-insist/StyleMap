2556754a0d832311fbb0b1276a902bf9d28d7f2e
345 <lv345_y@163.com>
2021-04-07 22:14:07 +0800

    build：添加百度地图sdk

---
+++ app/src/main/java/com/style/map/core/storage/FileUtils.java
@@ -0,0 +1,35 @@
+package com.style.map.core.storage;
+
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileUtils {
+    public static String getAssetsCacheFile(Context context, String fileName)   {
+        File cacheFile = new File(context.getCacheDir(), fileName);
+        try {
+            InputStream inputStream = context.getAssets().open(fileName);
+            try {
+                FileOutputStream outputStream = new FileOutputStream(cacheFile);
+                try {
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = inputStream.read(buf)) > 0) {
+                        outputStream.write(buf, 0, len);
+                    }
+                } finally {
+                    outputStream.close();
+                }
+            } finally {
+                inputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cacheFile.getAbsolutePath();
+    }
+}
--- app/src/main/java/com/style/map/ui/main/MainActivity.java
+++ app/src/main/java/com/style/map/ui/main/MainActivity.java
@@ -2,10 +2,13 @@ package com.style.map.ui.main;
 
 
 import com.style.map.R;
+import com.style.map.core.storage.FileUtils;
 import com.style.map.core.ui.BaseActivity;
 import com.style.map.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private static final String CUSTOM_FILE_NAME_CX = "7d09ae342b1b516719ee0321c2c29f50.sty";
 
     @Override
     public int layout() {
@@ -14,6 +17,30 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     public void initView() {
+        //设置个性皮肤
+        String customStyleFilePath = FileUtils.getAssetsCacheFile(this,CUSTOM_FILE_NAME_CX);
+        // 设置个性化地图样式文件的路径和加载方式
+        binding.mapView.setMapCustomStylePath(customStyleFilePath);
+        // 动态设置个性化地图样式是否生效
+        binding.mapView.setMapCustomStyleEnable(true);
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        binding.mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding.mapView.onDestroy();
+    }
 }
